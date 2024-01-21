36c07a443de1c99cb4e57b532670358fb8a665fc
Kihy <kh695640204@gmail.com>
2024-01-22 06:47:39 +1000

    update code

--- commands.sh
+++ commands.sh
@@ -1,1 +1,1 @@
-docker run --gpus all -it --rm -v "D:\liuer-mihou":/liuer-mihou kihy/deepo-nids bash
+docker run --gpus all -it --rm -v "D:\liuer-mihou":/workspace/liuer-mihou -v "D:\datasets":/workspace/datasets kihy/deepo-nids bash
